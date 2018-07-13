59cf2113b46e056f6f238c0894096d8ee50d8e7e
JulianChu <yulang.chu@gmail.com>
2018-07-13 22:04:43 +0200

    add page-create-account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { PageCreateAccountComponent } from './page-create-account/page-create-account.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageCreateAccountComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
