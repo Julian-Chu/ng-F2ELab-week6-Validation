681d289ecec288b46ca64f60af198e795bbfeccd
JulianChu <yulang.chu@gmail.com>
2018-07-13 22:59:07 +0200

    add page-payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from "./app.component";
 import { PageCreateAccountComponent } from "./page-create-account/page-create-account.component";
 import { PageGeneralInformationComponent } from "./page-general-information/page-general-information.component";
 import { PageUpdateProfilePictureComponent } from "./page-update-profile-picture/page-update-profile-picture.component";
+import { PagePaymentComponent } from "./page-payment/page-payment.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     PageCreateAccountComponent,
     PageGeneralInformationComponent,
-    PageUpdateProfilePictureComponent
+    PageUpdateProfilePictureComponent,
+    PagePaymentComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
