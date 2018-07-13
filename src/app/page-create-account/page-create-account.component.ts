d67a042be88d901b3c464570e07fae5f25cc7ce4
JulianChu <yulang.chu@gmail.com>
2018-07-13 22:11:24 +0200

    update style and html

--- src/app/page-create-account/page-create-account.component.ts
+++ src/app/page-create-account/page-create-account.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-page-create-account',
-  templateUrl: './page-create-account.component.html',
-  styleUrls: ['./page-create-account.component.css']
+  selector: "app-page-create-account",
+  templateUrl: "./page-create-account.component.html",
+  styleUrls: ["./page-create-account.component.css"]
 })
 export class PageCreateAccountComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
