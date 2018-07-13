93746f2e3529fb0230e8b3a1e46d2bb20e72b526
JulianChu <yulang.chu@gmail.com>
2018-07-13 22:20:04 +0200

    add page-general-information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { PageCreateAccountComponent } from './page-create-account/page-create-account.component';
+import { PageGeneralInformationComponent } from './page-general-information/page-general-information.component';
 
 @NgModule({
-  declarations: [AppComponent, PageCreateAccountComponent],
+  declarations: [AppComponent, PageCreateAccountComponent, PageGeneralInformationComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-general-information/page-general-information.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-page-general-information",
+  templateUrl: "./page-general-information.component.html",
+  styleUrls: ["./page-general-information.component.css"]
+})
+export class PageGeneralInformationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
