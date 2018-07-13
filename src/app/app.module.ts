3831aceb59d78e3e5aebea2b48834b44af2ae38d
JulianChu <yulang.chu@gmail.com>
2018-07-13 22:51:32 +0200

    add page-update-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { PageCreateAccountComponent } from './page-create-account/page-create-account.component';
-import { PageGeneralInformationComponent } from './page-general-information/page-general-information.component';
+import { PageCreateAccountComponent } from "./page-create-account/page-create-account.component";
+import { PageGeneralInformationComponent } from "./page-general-information/page-general-information.component";
+import { PageUpdateProfilePictureComponent } from "./page-update-profile-picture/page-update-profile-picture.component";
 
 @NgModule({
-  declarations: [AppComponent, PageCreateAccountComponent, PageGeneralInformationComponent],
+  declarations: [
+    AppComponent,
+    PageCreateAccountComponent,
+    PageGeneralInformationComponent,
+    PageUpdateProfilePictureComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-update-profile-picture/page-update-profile-picture.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-page-update-profile-picture",
+  templateUrl: "./page-update-profile-picture.component.html",
+  styleUrls: ["./page-update-profile-picture.component.css"]
+})
+export class PageUpdateProfilePictureComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
