0c3e05c2ec7ea500021334c60ed47ecb6ff339ea
JulianChu <yulang.chu@gmail.com>
2018-07-14 12:59:58 +0200

    add default validators

--- src/app/page-create-account/page-create-account.component.ts
+++ src/app/page-create-account/page-create-account.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  AbstractControl
+} from "@angular/forms";
 
 @Component({
   selector: "app-page-create-account",
@@ -8,18 +14,26 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 })
 export class PageCreateAccountComponent implements OnInit {
   form: FormGroup;
+  account: AbstractControl;
+  password: AbstractControl;
+  repassword: AbstractControl;
   @Output() toNextPage = new EventEmitter();
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      account: ["11", [Validators.required]],
-      password: null,
-      repassword: null
+      account: ["", [Validators.required]],
+      password: [null, [Validators.required]],
+      repassword: [null, [Validators.required]]
     });
+
+    this.account = this.form.get("account");
+    this.password = this.form.get("password");
+    this.repassword = this.form.get("repassword");
   }
 
   goToNextPage() {
+    console.log(this.form.errors);
     console.log(this.form.value);
     this.toNextPage.emit(this.form.value);
   }
