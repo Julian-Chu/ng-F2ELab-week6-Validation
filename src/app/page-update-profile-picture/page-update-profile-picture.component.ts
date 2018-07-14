ec6b15be3843f7db4c09761e33270de43e08bc18
JulianChu <yulang.chu@gmail.com>
2018-07-14 23:16:46 +0200

    implment drag and drop

--- src/app/page-update-profile-picture/page-update-profile-picture.component.ts
+++ src/app/page-update-profile-picture/page-update-profile-picture.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef } from "@angular/core";
+import { Observable } from "../../../node_modules/rxjs";
 
 @Component({
   selector: "app-page-update-profile-picture",
@@ -6,7 +7,65 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./page-update-profile-picture.component.css"]
 })
 export class PageUpdateProfilePictureComponent implements OnInit {
+  dragging = false;
+  images: Array<any> = [];
   constructor() {}
 
   ngOnInit() {}
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    const data = event.dataTransfer;
+    const files = data.files;
+    this.dragging = false;
+    const fileArray = Array.from(files);
+    fileArray.forEach(file => {
+      // this.previewImage(file).subscribe(img => {
+      //   console.log(img.src);
+      //   this.images.push(img.src);
+      //   console.log(this.images);
+      // });
+      this.previewImage(file);
+    });
+  }
+
+  previewImage(file: File) {
+    const pattern = new RegExp(/image-*/);
+    if (!file.type.match(pattern)) {
+      alert(`${File.name} not image`);
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    // return Observable.create(observer => {
+    //   reader.onloadend = () => {
+    //     observer.next(reader.result);
+    //     observer.complete();
+    //   };
+    // });
+
+    reader.onloadend = () => {
+      const src = reader.result;
+      this.images.push(src);
+    };
+  }
+
+  onDragenter(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dragging = true;
+  }
+
+  onDragleave(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = false;
+  }
+
+  onDragover(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = true;
+  }
+
+  deleteImage(index) {
+    this.images.splice(index, 1);
+  }
 }
